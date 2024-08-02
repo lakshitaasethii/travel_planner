df940f62ea0c7a0e5692248a410ec645207b427c
Chris <118865144+ChrisMillette@users.noreply.github.com>
2024-08-02 03:59:22 -0600

    Update retrievePackage.js

--- src/js/retrievePackage.js
+++ src/js/retrievePackage.js
@@ -2,7 +2,7 @@
 import { user, checkAuthState } from "./auth-context.js";
 
 document.addEventListener("DOMContentLoaded", async function () {
-    await checkAuthState(); // Ensure auth state is checked before proceeding
+    await checkAuthState();
     const currentUser = user.get();
     fetch('http://ec2-15-223-1-70.ca-central-1.compute.amazonaws.com:3000/getPackages')
         .then(response => response.json())
@@ -14,52 +14,53 @@ document.addEventListener("DOMContentLoaded", async function () {
                     const resultItem = document.createElement('div');
                     resultItem.classList.add('package-card');
                     resultItem.innerHTML = `
-                            <figure class="card-banner">
-                                <img src="${packageItem.imgRef}" alt="${packageItem.name}" loading="lazy">
-                            </figure>
+                        <figure class="card-banner">
+                            <img src="${packageItem.imgRef}" alt="${packageItem.name}" loading="lazy">
+                        </figure>
 
-                            <div class="card-content">
-                                <h3 class="h3 card-title">${packageItem.name}</h3>
-                                <p class="card-text">${packageItem.description}</p>
-                                <ul class="card-meta-list">
-                                    <li class="card-meta-item">
-                                        <div class="meta-box">
-                                            <ion-icon name="time"></ion-icon>
-                                            <p class="text">7D/6N</p>
-                                        </div>
-                                    </li>
-                                    <li class="card-meta-item">
-                                        <div class="meta-box">
-                                            <ion-icon name="people"></ion-icon>
-                                            <p class="text">pax: ${packageItem.partySize}</p>
-                                        </div>
-                                    </li>
-                                    <li class="card-meta-item">
-                                        <div class="meta-box">
-                                            <ion-icon name="location"></ion-icon>
-                                            <p class="text">Destination</p>
-                                        </div>
-                                    </li>
-                                </ul>
-                            </div>
-
-                            <div class="card-price">
-                                <div class="wrapper">
-                                    <p class="reviews">(20 reviews)</p>
-                                    <div class="card-rating">
-                                        <ion-icon name="star"></ion-icon>
-                                        <ion-icon name="star"></ion-icon>
-                                        <ion-icon name="star"></ion-icon>
-                                        <ion-icon name="star"></ion-icon>
-                                        <ion-icon name="star"></ion-icon>
+                        <div class="card-content">
+                            <h3 class="h3 card-title">${packageItem.name}</h3>
+                            <p class="card-text">${packageItem.description}</p>
+                            <ul class="card-meta-list">
+                                <li class="card-meta-item">
+                                    <div class="meta-box">
+                                        <ion-icon name="time"></ion-icon>
+                                        <p class="text">${packageItem.duration}</p>
                                     </div>
+                                </li>
+                                <li class="card-meta-item">
+                                    <div class="meta-box">
+                                        <ion-icon name="people"></ion-icon>
+                                        <p class="text">#: ${packageItem.partySize}</p>
+                                    </div>
+                                </li>
+                                <li class="card-meta-item">
+                                    <div class="meta-box">
+                                        <ion-icon name="location"></ion-icon>
+                                        <p class="text">${packageItem.city}</p>
+                                    </div>
+                                </li>
+                            </ul>
+                        </div>
+
+                        <div class="card-price">
+                            <div class="wrapper">
+                                <p class="reviews">(20 reviews)</p>
+                                <div class="card-rating">
+                                    <ion-icon name="star"></ion-icon>
+                                    <ion-icon name="star"></ion-icon>
+                                    <ion-icon name="star"></ion-icon>
+                                    <ion-icon name="star"></ion-icon>
+                                    <ion-icon name="star"></ion-icon>
                                 </div>
-                                <p class="price">
-                                    $${packageItem.price}
-                                    <span>/ per person</span>
-                                </p>
-                                <button class="btn btn-secondary" onclick="location.href='${currentUser ? `book.html?id=${encodeURIComponent(packageItem.id)}` : 'signin.html'}'">Book Now</button>
                             </div>
+                            <p class="price">
+                                $${packageItem.price}
+                                <span>/ per person</span>
+                            </p>
+                            <button class="btn btn-primary" onclick="learnMore('${encodeURIComponent(JSON.stringify(packageItem))}')">Learn More</button>
+                            <button class="btn btn-secondary" onclick="bookNow('${encodeURIComponent(JSON.stringify(packageItem))}')">Book Now</button>
+                        </div>
                     `;
                     resultsDiv.appendChild(resultItem);
                 });
@@ -69,3 +70,15 @@ document.addEventListener("DOMContentLoaded", async function () {
         })
         .catch(error => console.error('Fetch error:', error));
 });
+
+window.learnMore = function (packageItem) {
+    const data = JSON.parse(decodeURIComponent(packageItem));
+    localStorage.setItem('selectedPackage', JSON.stringify(data));
+    window.location.href = 'package.html';
+}
+
+window.bookNow = function (packageItem) {
+    const data = JSON.parse(decodeURIComponent(packageItem));
+    localStorage.setItem('selectedPackage', JSON.stringify(data));
+    window.location.href = 'packageBooking.html';
+}
